40761587cfb6479a5170508fa7806fa4c44473d8
Liran Martfel <liran.martfel@gmail.com>
2026-01-26 16:04:56 +0200

    Add files via upload

---
+++ Homework_rectangle_of_asterisk.py
@@ -0,0 +1,15 @@
+c: int = int(input("Enter the column number: "))
+r: int = int(input("Enter the row number: "))
+d = 1
+
+print ("*"*c)
+#print column
+
+while r > d:
+#as long as rows if higher than d (the counter)
+
+    print ("*"*c)
+#print the same amount of column in a new row
+
+    d += 1
+#keeps the numbers of rows right
